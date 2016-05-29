56649c8cb8b17190501ba3ebc5c646b1863a0f6e
Ole Jørgen Brønner <olejorgenb@yahoo.no>
2016-05-29 15:24:07 +0200

    :

--- default.nix
+++ default.nix
@@ -2,12 +2,20 @@ with import <nixpkgs> {};
 
 # This includes some private/local-source packages but since nix is a lazy
 # evaluated language there's no error unless the package is attempted installed
+#
+# Note that notion also use a hard-coded source path
 
 rec {
   data_hacks = callPackage ./data_hacks {} ;
+
   gpp = callPackage ./gpp {} ;
+
   idle_logger = callPackage ~/src/idle_logger {} ;
+
   zsh-completions = callPackage ./zsh-completions {} ;
+
+  # Forked from the official nixpkgs
   notion = callPackage ./notion { enableXft = false; } ;
+
   selfspy = callPackage ./selfspy {} ;
 }
