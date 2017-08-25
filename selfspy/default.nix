310a949a36b5ddbcf8e1839f4fd676e46b891435
Ole Jørgen Brønner <olejorgenb@yahoo.no>
2017-08-25 12:33:55 +0200

    selfspy: cleanup

--- selfspy/default.nix
+++ selfspy/default.nix
@@ -1,29 +1,35 @@
- {
-  pythonPackages
- }:
+ { stdenv, fetchgit, pythonPackages, python }:
 
 
-pythonPackages. buildPythonPackage {
-  name = "selfspy";
-  # buildPythonPackage build for each interpreter unless 'disabled' are true
+pythonPackages.buildPythonPackage rec {
+  name = "selfspy-${version}";
+  version = "unstable-2016-02-07";
+
+  # buildPythonPackage builds for each interpreter unless 'disabled' are true
   # Famous last words, but we'll do it simple now and only build for 2.7
   # 3.x is not supported
   disabled = !(pythonPackages.isPy27);
 
-  src = ~/src/selfspy;
+  src = fetchgit {
+    url = "https://github.com/gurgeh/selfspy.git";
+    rev = "a98a9c06d14b6f8b6ad76948ab75a2f09c89c578";
+    sha256 = "0d0qvq7s229pcdp11bpwgganj94yh6c22s8qyb0gb7vcn3dydxb3";
+  };
 
   propagatedBuildInputs = with pythonPackages; [
+    python
     xlib
-    sqlalchemy9
+    sqlalchemy
     lockfile
     pycrypto
     keyring
     tkinter
   ];
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Log everything you do on the computer, for statistics, future reference and all-round fun!";
     homepage = "https://github.com/gurgeh/selfspy";
-    license = "GPLv3";
+    maintainers = [ maintainers.olejorgenb ];
+    license = licenses.gpl3;
   };
 }
